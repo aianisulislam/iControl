cd7055c826a1afe9523d47c857ab0ac8ff48d063
Anis <aianisulislam@gmail.com>
2026-03-10 03:29:52 +0530

    Initial Commit

---
+++ iControl/iControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  iControlApp.swift
+//  iControl
+//
+//  Created by Anisul Islam on 10/03/26.
+//
+
+import SwiftUI
+
+@main
+struct iControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
